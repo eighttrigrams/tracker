c7a01eb20a823023d3f7ed5a76aa4303045ff457
Dan <dan@eighttrigrams.net>
2022-12-18 16:59:00 +0000

    Show only one when selected context

--- src/cljs/main/sides.cljs
+++ src/cljs/main/sides.cljs
@@ -2,7 +2,7 @@
   (:require [main.sides.lhs :as lhs]
             [main.sides.rhs :as rhs]))
 
-(defn component [state]
+(defn component [state*]
   [:<>
-   [:div#lhs-component.list-component [lhs/component state]] 
-   [:div#rhs-component.list-component [rhs/component state]]])
+   [:div#lhs-component.list-component [lhs/component state*]] 
+   [:div#rhs-component.list-component [rhs/component state*]]])
--- src/cljs/main/sides/lhs.cljs
+++ src/cljs/main/sides/lhs.cljs
@@ -1,33 +1,42 @@
 (ns main.sides.lhs
   (:require repository))
 
-(defn- handle-event [state f]
+(defn- handle-event [*state f]
   (fn [e]
-    (f #(reset! state %) (-> e .-target .-value))))
+    (f #(reset! *state %) (-> e .-target .-value))))
 
-(defn- on-click-item [state context]
-  (handle-event state
+(defn- on-click-item [*state context]
+  (handle-event *state
                 (fn [reset! _value]
                   (repository/fetch!
-                   (assoc @state :selected-context context)
+                   (assoc @*state :selected-context context)
                    "" reset!))))
 
-(defn- contexts-list [state]
-  [:ul
-   (doall (for [context (:contexts @state)]
-            ^{:key (:id context)}
-            [:li
-             {:class (when (= (:selected-context @state) context) :selected)
-              :on-click (on-click-item state context)}
-             (:title context)]))])
+(defn- list-item [*state context]
+  [:li
+   {:class    (when (= (:selected-context @*state) context) :selected)
+    :on-click (on-click-item *state context)}
+   (:title context)])
 
-(defn component [_state]
-  (fn [state]
-    (if (:selected-issue @state)
+(defn- contexts-list [*state]
+  [:ul
+   (doall 
+    (for [context (:contexts @*state)]
+      ^{:key (:id context)}
+      [list-item *state context]))])
+
+(defn component [_*state]
+  (fn [*state]
+    (cond 
+      (:selected-issue @*state)
       [:<> 
-       [:h1 (:title (:selected-issue @state))]
-       [:p (:description (:selected-issue @state))]]
+       [:h1 (:title (:selected-issue @*state))]
+       [:p (:description (:selected-issue @*state))]]
+      (:selected-context @*state)
+      [:ul 
+       [list-item *state (:selected-context @*state)]]
+      :else
       [:<>
        #_(when (= :issues (:active-search @state))
            [input-component state])
-       [contexts-list state]])))
+       [contexts-list *state]])))
