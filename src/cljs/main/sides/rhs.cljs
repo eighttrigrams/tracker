aa844cb7d90037134c097b747d94985e51cb9f03
Dan <dan@eighttrigrams.net>
2022-12-18 11:34:52 +0000

    Focus input on open

--- src/cljs/main/sides/rhs.cljs
+++ src/cljs/main/sides/rhs.cljs
@@ -1,14 +1,24 @@
 (ns main.sides.rhs
-  (:require repository))
+  (:require [reagent.core :as r]
+            repository))
+
+(defn- input-component [state]
+  (r/create-class 
+   {:component-did-mount #(.focus (.getElementById js/document "issues-search-input"))
+    :render (fn []
+              [:input#issues-search-input
+               {:on-change (fn [e]
+                             (repository/fetch! state (.-value (.-target e))))}])}))
+
+(defn- issues-list [state]
+  [:ul
+   (for [issue (:issues @state)]
+     ^{:key (:id issue)}
+     [:li (:title issue)])])
 
 (defn component [_state]
   (fn [state]
     [:<>
      (when (:issues-search-active? @state)
-       [:input 
-        {:on-change (fn [e]
-                      #_(swap! state (fn [old-state] (assoc old-state :q (.-value (.-target e)))))
-                      (repository/fetch! state (.-value (.-target e))))}])
-     [:ul
-      (map (fn [%] [:li {:key (:id %)} (:title %)])
-           (:issues @state))]]))
+       [input-component state])
+     [issues-list state]]))
