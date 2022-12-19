91b19bc91332025d5abc642235152e5a110d1dd1
Dan <dan@eighttrigrams.net>
2022-12-19 15:22:18 +0000

    Refactor

--- src/cljs/ui/main/rhs.cljs
+++ src/cljs/ui/main/rhs.cljs
@@ -3,14 +3,17 @@
             [ui.actions :as actions]
             [ui.main.input :as input]))
 
-(defn- issues-list [*state]
+(defn- issues-list-item-component [*state issue]
+  [:li
+   {:class    (when (= (:selected-issue @*state) issue) :selected)
+    :on-click #(actions/select-issue! *state issue)}
+   (:title issue)])
+
+(defn- issues-list-component [*state]
   [:ul
    (doall (for [issue (:issues @*state)]
             ^{:key (:id issue)}
-            [:li 
-             {:class    (when (= (:selected-issue @*state) issue) :selected)
-              :on-click #(actions/select-issue! *state issue)}
-             (:title issue)]))])
+            [issues-list-item-component *state issue]))])
 
 (defn component [_*state]
   (fn [*state]
@@ -19,4 +22,4 @@
        [input/component *state])
      [:div.list-component
       {:class (when (= :issues (:active-search @*state)) :search-active)}
-      [issues-list *state]]]))
+      [issues-list-component *state]]]))
