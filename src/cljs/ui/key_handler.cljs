4513547bfaca03465cbf19809f98a8ad830c731c
Dan <dan@eighttrigrams.net>
2022-12-20 10:44:27 +0000

    Simplify handle-keys

--- src/cljs/ui/key_handler.cljs
+++ src/cljs/ui/key_handler.cljs
@@ -3,37 +3,34 @@
 
 (defn- handle-keys* [f]
   (fn [e]
-    (let [code           (.-code e)
+    (let [code          (.-code e)
           ctrl-pressed? (.-ctrlKey e)]
       (f code ctrl-pressed? e))))
 
 (defn handle-keys [*state]
   (handle-keys* 
    (fn [code _ctrl-pressed? _e]
-     (cond (and (not (:active-search @*state))
-                (or (:selected-issue @*state)
-                    (:selected-context @*state))
-                (= "KeyD" code))
+     (cond (= "Escape" code)
+           (cond (:active-search @*state)
+                 (actions/quit-search! *state)
+                 (:selected-issue @*state)
+                 (swap! *state #(dissoc % :selected-issue))
+                 (:selected-context @*state)
+                 (actions/deselect-context! *state))
+           (and 
+            (or 
+             (:selected-issue @*state)
+             (:selected-context @*state))
+            (= "KeyD" code))
            (swap! *state #(assoc % :modal :description))
-           (and (not (:active-search @*state))
-                (= "KeyI" code))
-           (swap! *state (fn [old-state] (assoc old-state :active-search :issues)))
-           (and (not (:active-search @*state))
-                (= "KeyC" code))
-           (swap! *state (fn [old-state] (assoc old-state :active-search :contexts)))
-           (and (not (:active-search @*state))
-                (:selected-context @*state)
-                (= "KeyN" code))
-           (swap! *state #(assoc % :modal :new-issue))
-           (and (:active-search @*state)
-                (= "Escape" code))
-           (actions/quit-search! *state)
-           (and (:selected-issue @*state)
-                (= "Escape" code))
-           (swap! *state (fn [old-state] (dissoc old-state :selected-issue)))
-           (and (:selected-context @*state)
-                (= "Escape" code))
-           (actions/deselect-context! *state)))))
+           (= "KeyI" code)
+           (swap! *state #(assoc % :active-search :issues))
+           (= "KeyC" code)
+           (swap! *state #(assoc % :active-search :contexts))
+           (and 
+            (:selected-context @*state)
+            (= "KeyN" code))
+           (swap! *state #(assoc % :modal :new-issue))))))
 
 (defn handle-modal-keys [*state id value-fn]
   (handle-keys*
