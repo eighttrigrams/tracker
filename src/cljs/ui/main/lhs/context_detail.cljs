6b3580cd563a3fd6bf92c3297675884f15ba8541
Dan <dan@eighttrigrams.net>
2022-12-21 17:08:25 +0000

    Display context description as markdown

--- src/cljs/ui/main/lhs/context_detail.cljs
+++ src/cljs/ui/main/lhs/context_detail.cljs
@@ -1,9 +1,11 @@
 (ns ui.main.lhs.context-detail
-  (:require [ui.main.lhs.list-item :as list-item]))
+  (:require [ui.main.lhs.list-item :as list-item]
+            ["react-markdown$default" :as ReactMarkdown]))
 
 (defn item-component [*state]
   [:ul.cards
    [list-item/component *state (:selected-context @*state)]])
 
 (defn component [*state]
-  [:<> (:description (:selected-context @*state))])
+  [:> ReactMarkdown
+   {:children (:description (:selected-context @*state))}])
