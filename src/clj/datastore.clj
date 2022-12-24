8aa6e115c960aa171c7f7c10a5f4f0245ec3bb7e
Dan <dan@eighttrigrams.net>
2022-12-24 11:32:13 +0000

    Clean up a bit

--- src/clj/datastore.clj
+++ src/clj/datastore.clj
@@ -18,34 +18,35 @@
 ;; TODO in minimals, show examples which use substitution/formatting
 
 (defn new-issue [db value context-id]
-  (let [issue (un-namespace-keys 
-               (jdbc/execute-one! db
-                                  (sql/format {:insert-into [:issues]
-                                               :columns     [:inserted_at :updated_at :title]
-                                               :values      [[[:raw "NOW()"] [:raw "NOW()"] value]]})
-                                  
-                                  {:return-keys true}))]
+  (let [issue
+        (jdbc/execute-one! db
+                           (sql/format {:insert-into [:issues]
+                                        :columns     [:inserted_at :updated_at :title]
+                                        :values      [[[:raw "NOW()"] [:raw "NOW()"] value]]})
+                           
+                           {:return-keys true})]
     (jdbc/execute! db
                    (sql/format {:insert-into [:context-issue]
                                 :columns [:context_id :issue_id]
                                 :values [[[:inline context-id]
-                                          [:inline (:id issue)]]]}))
-    issue))
+                                          [:inline (:issues/id issue)]]]}))
+    (-> issue
+        un-namespace-keys
+        (dissoc :searchable))))
 
 (defn update-issue [db id value]
   (jdbc/execute! db
                  (sql/format {:delete-from [:events]
                               :where [:= :issue_id [:inline id]]}))
-  (let [issue 
-        (un-namespace-keys
-         (jdbc/execute-one! db
-                            (sql/format {:update [:issues]
-                                         :where  [:= :id [:inline id]]
-                                         :set    {:title       [:inline (:title value)]
-                                                  :short_title [:inline (:short_title value)]
-                                                  :tags        [:inline (:tags value)]
-                                                  :updated_at  [:raw "NOW()"]}})
-                            {:return-keys true}))]
+  (let [issue
+        (jdbc/execute-one! db
+                           (sql/format {:update [:issues]
+                                        :where  [:= :id [:inline id]]
+                                        :set    {:title       [:inline (:title value)]
+                                                 :short_title [:inline (:short_title value)]
+                                                 :tags        [:inline (:tags value)]
+                                                 :updated_at  [:raw "NOW()"]}})
+                           {:return-keys true})]
     (when (:date value)
       (jdbc/execute! db
                      (sql/format {:insert-into [:events]
@@ -54,31 +55,34 @@
                                                  [:inline (:date value)]
                                                  [:raw "NOW()"]
                                                  [:raw "NOW()"]]]})))
-    issue))
+    (-> issue
+        un-namespace-keys
+        (dissoc :searchable))))
 
 (defn update-issue-description [ds id value]
-  (jdbc/execute! ds
-                 (sql/format {:update [:issues]
-                              :set    {:description [:inline value]
-                                       :updated_at [:raw "NOW()"]}
-                              :where  [:= :id [:inline id]]}))
-  ;; TODO dedup, see above
-  (un-namespace-keys
-   (first (jdbc/execute! ds
+  (jdbc/execute-one! ds
+                     (sql/format {:update [:issues]
+                                  :set    {:description [:inline value]
+                                           :updated_at [:raw "NOW()"]}
+                                  :where  [:= :id [:inline id]]}))
+  (-> (jdbc/execute-one! ds
                          (sql/format {:select :*
                                       :from [:issues]
-                                      :where [:= :id [:inline id]]})))))
+                                      :where [:= :id [:inline id]]}))
+      un-namespace-keys
+      (dissoc :searchable)))
 
 ;; TODO dedup, see above
 (defn update-context-description [ds id value]
-  (jdbc/execute! ds
-                 (sql/format {:update [:contexts]
-                              :set    {:description [:inline value]
-                                       :updated_at [:raw "NOW()"]}
-                              :where  [:= :id [:inline id]]}))
-  ;; TODO dedup, see above
-  (un-namespace-keys
-   (first (jdbc/execute! ds
+  (jdbc/execute-one! ds
+                     (sql/format {:update [:contexts]
+                                  :set    {:description [:inline value]
+                                           :updated_at [:raw "NOW()"]}
+                                  :where  [:= :id [:inline id]]}))
+  
+  (-> (jdbc/execute-one! ds
                          (sql/format {:select :*
                                       :from [:contexts]
-                                      :where [:= :id [:inline id]]})))))
+                                      :where [:= :id [:inline id]]}))
+      un-namespace-keys
+      (dissoc :searchable)))
