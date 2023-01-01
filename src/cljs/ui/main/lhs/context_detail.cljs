e0e7f0b7b69a3cbe82c3a88a9c4f8e7acfa7bad5
Dan <dan@eighttrigrams.net>
2023-01-01 19:47:12 +0000

    Count secondary contexts

--- src/cljs/ui/main/lhs/context_detail.cljs
+++ src/cljs/ui/main/lhs/context_detail.cljs
@@ -7,21 +7,40 @@
   [:ul.cards
    [list-item/component *state (:selected-context @*state)]])
 
+(defn count-issues [issues secondary-contexts]
+  (let [count-reducer,,
+        #(fn [count issue]
+          (if (contains? (:contexts issue) %)
+            (inc count)
+            count))]
+    (map (fn [[id title]]
+           [id
+            [title
+             (reduce (count-reducer id) 0 issues)]])
+         secondary-contexts)))
+
+(defn- select-secondary-context [*state id]
+  (fn [_]
+    (swap! *state update :selected-secondary-contexts-ids
+           #((if (contains? % id) disj conj) % id))
+    (actions/change-secondary-contexts-selection! *state)))
+
 (defn- secondary-contexts-component [*state]
-  (let [{:keys                        [selected-secondary-contexts-ids]
+  (let [{:keys                        [selected-secondary-contexts-ids
+                                       issues]
          {:keys [secondary_contexts]} :selected-context} @*state]
     [:ul
-     (doall
-      (map (fn [[id title]]
-             [:li
-              {:key id
-               :on-click (fn [_]
-                           (swap! *state update :selected-secondary-contexts-ids 
-                                  #((if (contains? % id) disj conj) % id))
-                           (actions/change-secondary-contexts-selection! *state))} 
-              (when (contains? selected-secondary-contexts-ids id) "!")
-              title])
-           secondary_contexts))]))
+     (->> secondary_contexts
+          (count-issues issues)
+          (sort-by (fn [[_id [_title count]]] count))
+          reverse
+          (map (fn [[id [title count]]]
+                 [:li
+                  {:key id
+                   :on-click (select-secondary-context *state id)} 
+                  (when (contains? selected-secondary-contexts-ids id) "!")
+                  title
+                  (str " (" count ")")])))]))
 
 (defn component [_*state]
   (fn [*state]
@@ -29,5 +48,6 @@
      [:h2 "Search mode: " (:search_mode (:selected-context @*state))]
      [:h2 "Secondary contexts:"]
      [secondary-contexts-component *state]
+     [:hr]
      [:> ReactMarkdown
       {:children (:description (:selected-context @*state))}]]))
