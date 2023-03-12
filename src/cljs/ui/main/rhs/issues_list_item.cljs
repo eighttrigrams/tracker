82b69d67b915182c8cbf77c3fd1562c4ec6b280b
danielmarreirosdeoliveira <dan@eighttrigrams.net>
2023-03-12 14:56:58 +0000

    Extract components; set badge text color explicitely

--- src/cljs/ui/main/rhs/issues_list_item.cljs
+++ src/cljs/ui/main/rhs/issues_list_item.cljs
@@ -2,6 +2,34 @@
   (:require [ui.actions :as actions]
             ["react-markdown$default" :as ReactMarkdown]))
 
+(defn- context-badges-component [state contexts]
+  [:span.contexts
+   (doall
+    (->> contexts
+         (filter (fn [[idx _title]]
+                   (not= idx (:id (:selected-context state)))))
+         (map (fn [[idx title]]
+                [:span.badge {:key idx}
+                 title]))))])
+
+(defn- info-component [state issue]
+  [:span.info
+   (when (and (:selected-context state)
+              (not= 0 (:search_mode (:selected-context state)))
+              (not (and (= (:short_title_ints issue) 0)
+                        (empty? (:short_title issue)))))
+     (str "["
+          (if (> (:short_title_ints issue) 0)
+            (:short_title_ints issue)
+            (:short_title issue))
+          "] "))
+   [:span.date (:date issue)]])
+
+(defn- title-component [title]
+  [:span.title
+   [:> ReactMarkdown
+    {:children title}]])
+
 (defn component [*state issue]
   [:li.card.issue-card
    {:class     (when (= (:id (:selected-issue @*state))
@@ -9,27 +37,6 @@
     :on-click #(actions/select-issue! *state issue)}
    [:div
     {:class (when (:important issue) :important)}
-    [:span.title
-     [:> ReactMarkdown
-      {:children (:title issue)}]]
-    
-    [:span.info
-     (when (and (:selected-context @*state)
-                (not= 0 (:search_mode (:selected-context @*state)))
-                (not (and (= (:short_title_ints issue) 0)
-                          (empty? (:short_title issue)))))
-       (str "["
-            (if (> (:short_title_ints issue) 0)
-              (:short_title_ints issue)
-              (:short_title issue))
-            "] "))
-     [:span.date (:date issue)]]
-    
-    [:span.contexts
-     (doall
-      (->> (:contexts issue)
-           (filter (fn [[idx _title]]
-                     (not= idx (:id (:selected-context @*state)))))
-           (map (fn [[idx title]]
-                  [:span.badge {:key idx}
-                   title]))))]]])
+    [title-component (:title issue)]
+    [info-component @*state issue]
+    [context-badges-component @*state (:contexts issue)]]])
