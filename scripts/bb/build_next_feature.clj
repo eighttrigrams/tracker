ee74c270dbc6680e45687594d62ec07acfce50a7
Dan <dan@eighttrigrams.net>
2026-01-26 18:14:58 +0000

    Add port

--- scripts/bb/build_next_feature.clj
+++ scripts/bb/build_next_feature.clj
@@ -59,7 +59,7 @@
     (send-notification "Build Started" (str "Building feature: " feature-name))
     (log "Start building ...")
 
-    (shell "builder" "fullstack-exploratory-first-with-boyscout-help" branch-name)
+    (shell "builder" "fullstack-exploratory-first-with-boyscout-help" branch-name "3027")
 
     (log "Done!")
 
