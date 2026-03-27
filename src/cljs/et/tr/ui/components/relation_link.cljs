cbdee315a41882cd47dabb3f2da90b785541297f
Dan <dan@eighttrigrams.net>
2026-03-27 16:11:36 +0000

    Indicate which items are already linked

--- src/cljs/et/tr/ui/components/relation_link.cljs
+++ src/cljs/et/tr/ui/components/relation_link.cljs
@@ -5,13 +5,28 @@
   (or (= 1 (:today task))
       (:due_date task)))
 
+(def ^:private item-type->collection-key
+  {:task :tasks
+   :resource :resources
+   :meet :meets})
+
+(defn- already-linked? [source item-type item-id]
+  (when source
+    (let [coll-key (item-type->collection-key item-type)
+          items (get @state/*app-state coll-key)
+          item (first (filter #(= (:id %) item-id) items))]
+      (some #(and (= (:type %) (:type source))
+                  (= (:id %) (:id source)))
+            (:relations item)))))
+
 (defn relation-link-button [item-type item-id]
   (when (state/relation-mode-active?)
     (let [source (state/relation-source)]
-      (when-not (and (= "today" (:type source))
-                     (or (not= :task item-type)
-                         (when-let [task (first (filter #(= (:id %) item-id) (:tasks @state/*app-state)))]
-                           (task-ineligible-for-today? task))))
+      (when-not (or (and (= "today" (:type source))
+                         (or (not= :task item-type)
+                             (when-let [task (first (filter #(= (:id %) item-id) (:tasks @state/*app-state)))]
+                               (task-ineligible-for-today? task))))
+                    (already-linked? source item-type item-id))
         (let [is-source? (and source
                               (= (:type source) (state/item-type->prefix item-type))
                               (= (:id source) item-id))]
